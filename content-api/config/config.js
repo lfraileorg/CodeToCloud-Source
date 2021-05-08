76abcad9cdd09ffd89a7d54cb606a5a4310819d2
Luis Fraile <lfraile@lfraile.net>
2021-05-08 17:31:17 +0200

    Update config.js

--- content-api/config/config.js
+++ content-api/config/config.js
@@ -1,5 +1,5 @@
 exports.appSettings = {
     db: (function(){
-        return process.env.MONGODB_CONNECTION || 'mongodb://localhost:27017/contentdb';
+        return process.env.MONGODB_CONNECTION || 'mongodb://cosmoslfdemo:snOs7mLecAW3jUGH2s6vVPLHUC6TICh7zwiw7rH0sa1ZrGZaplZKZZOn41sG8qANHDpkPIvgTpxveoCvwdlilA==@cosmoslfdemo.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@cosmoslfdemo@';
     })()
 };
